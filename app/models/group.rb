0b7fdcc5eeca153ac08dc0ea8d38d083808085c7
tukiji723 <man.with.67suki@gmail.com>
2019-11-03 11:28:38 +0900

    access-view

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ApplicationRecord
-  has_many :groupa_users
-  has_many :users ,through: :groupa_users
-  calidaters :name, presence: true
+  has_many :group_users
+  has_many :users, through: :group_users
+  validates :name, presence: true, uniqueness: true
 end
