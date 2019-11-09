9065a0b708626762c86e46abb8555e523f42c00b
tukiji723 <man.with.67suki@gmail.com>
2019-11-09 15:33:54 +0900

    carrierwave

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,4 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'groups#index'
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update]
-    resources :messages, only: [:index, :create]
 end
